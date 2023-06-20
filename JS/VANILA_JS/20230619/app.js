ec5746aa7e946bf496b726cb94fbb147393232fc
kikiru328 <kikiru328@gmail.com>
2023-06-20 12:35:21 +0900

    JS"

--- JS/VANILA_JS/20230619/app.js
+++ JS/VANILA_JS/20230619/app.js
@@ -2,6 +2,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
 // click Event
 function onLoginSubmit(event) {
   // same as button
@@ -21,4 +22,9 @@ function onLoginSubmit(event) {
   //  Just for check
   console.log(username);
 }
+
+function handleLinkClick(event) {
+  alert("clicked");
+}
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", onLoginSubmit);
